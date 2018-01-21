2c8d4796c3be1a3b368ad833a6617215470263ac
mohamed <moh4med.ahmed@gmail.com>
2018-01-21 02:15:10 +0200

    update the UI & start to handle the operation.

--- app/src/main/java/com/example/mohamed/boocalculator/MainActivity.java
+++ app/src/main/java/com/example/mohamed/boocalculator/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.mohamed.boocalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements numbersFragment.numberListener {
     private static String TAG=MainActivity.class.getSimpleName();
+    TextView outputTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        outputTv=(TextView)findViewById(R.id.outputtv);
         Log.e(TAG,"ONCREATE");
+    }
+
+    @Override
+    public void OnClick(String s) {
+        outputTv.append(s);
+    }
+    public void opClick(View v) {
+        outputTv.append(((Button)v).getText());
+        return;
     }
 }
--- app/src/main/java/com/example/mohamed/boocalculator/numbersFragment.java
+++ app/src/main/java/com/example/mohamed/boocalculator/numbersFragment.java
@@ -9,12 +9,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class numbersFragment extends Fragment {
+public class numbersFragment extends Fragment implements View.OnClickListener {
     private static String TAG=numbersFragment.class.getSimpleName();
+    numberListener MnumberListener;
+    public interface numberListener{
+        void OnClick(String s);
+    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        this.MnumberListener=(numberListener) context;
         Log.e(TAG,"calling on attach");
     }
 
@@ -27,6 +33,7 @@ public class numbersFragment extends Fragment {
     public void onStart() {
         super.onStart();
         Log.e(TAG,"calling on start");
+
     }
 
     @Override
@@ -46,8 +53,35 @@ public class numbersFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Log.e(TAG,"calling oncreateView");
-        return inflater.inflate(R.layout.fragment_numbers, container, false);
+        View view = inflater.inflate(R.layout.fragment_numbers, container, false);
+        Button button0 = (Button) view.findViewById(R.id.button0);
+        button0.setOnClickListener(this);
+        Button button1 = (Button) view.findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = (Button) view.findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        Button button3 = (Button) view.findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        Button button4 = (Button) view.findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+        Button button5 = (Button) view.findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+        Button button6 = (Button) view.findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+        Button button7 = (Button) view.findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+        Button button8 = (Button) view.findViewById(R.id.button8);
+        button8.setOnClickListener(this);
+        Button button9 = (Button) view.findViewById(R.id.button9);
+        button9.setOnClickListener(this);
+        Button buttondot = (Button) view.findViewById(R.id.buttondot);
+        buttondot.setOnClickListener(this);
+        return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        MnumberListener.OnClick(((Button)v).getText().toString());
+    }
 }
