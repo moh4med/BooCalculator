d6037b78997e235e520751ba9b58c1dbc37cf7f8
mohamed <moh4med.ahmed@gmail.com>
2018-01-25 01:19:33 +0200

    fix the error for cursor position & restore state when rotate.
    (version 1 is ok for the program)

--- app/src/main/java/com/example/mohamed/boocalculator/MainActivity.java
+++ app/src/main/java/com/example/mohamed/boocalculator/MainActivity.java
@@ -17,6 +17,8 @@ import java.util.Random;
 import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity implements numbersFragment.numberListener {
+    private static final String INPUTSTRINGTAG ="INPUTSTRINGTAG" ;
+    private static final String HISTORYTAG ="HISTORYTAGTAG" ;
     private static String TAG = MainActivity.class.getSimpleName();
     EditText inputEText;
     TextView outputTv;
@@ -37,25 +39,36 @@ public class MainActivity extends AppCompatActivity implements numbersFragment.n
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
                 }
-                inputEText.requestFocus();
             }
         });
+        if(savedInstanceState!=null){               //restore the session data
+            String s=savedInstanceState.getString(INPUTSTRINGTAG);
+            inputEText.setText(s);
+        }
+        processOperation();
         Log.e(TAG, "ONCREATE");
     }
 
     @Override
     public void onNumClick(String s) {
-        inputEText.append(s);
+        changeinput(s,-1);
         processOperation();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(INPUTSTRINGTAG,inputEText.getText().toString());
+        //todo save stack data
+        super.onSaveInstanceState(outState);
     }
 
     public void opClick(View v) {
         String s = ((Button) v).getText().toString();
         String input = inputEText.getText().toString();
         if (s.equals(getString(R.string.buttondeltext))) {        // delete the char from input not added delete many
+            /* todo long press for clear the input */
             int chartodelete = inputEText.getSelectionStart() - 1;    //get start of the selection &delete it
             if (chartodelete >= 0) {
-                Log.e(TAG, "" + chartodelete);
                 input = input.substring(0, chartodelete) + input.substring(chartodelete + 1);
                 inputEText.setText(input);
                 inputEText.setSelection(chartodelete);
@@ -68,11 +81,11 @@ public class MainActivity extends AppCompatActivity implements numbersFragment.n
             inputEText.setSelection(output.length());
             outputTv.setText("");
         } else if (s.equals(getString(R.string.buttonDEGtext))) {  //not implemented yet
-            //pass
+            //todo implement deg/rad
         } else if (s.equals(getString(R.string.buttonrandom))) {
             Random random = new Random();
             int n = random.nextInt(234234);       // till now any random
-            inputEText.append("" + n);
+            changeinput(String.valueOf(n),-1);
         } else {
             addoperationtoinput(s);
         }
@@ -82,42 +95,45 @@ public class MainActivity extends AppCompatActivity implements numbersFragment.n
     }
 
     private void addoperationtoinput(String s) {    //how clicked button should appear on the screen
+        String newinput="";
+        int offset=-1;
         if (s.equals(getString(R.string.buttonmodtext))) {
-            inputEText.append("#");
+            newinput="#";
         } else if (s.equals(getString(R.string.buttonevaltext)) || s.equals(getString(R.string.buttonPitext))) {
-            inputEText.append(s);
+            newinput=s;
         } else if (s.equals(getString(R.string.buttonfactorialtext))) {
-            inputEText.append("!");
+            newinput="!";
         } else if (s.equals(getString(R.string.buttonsqrttext))) {
-            inputEText.append("()^(1/2)");
-            inputEText.setSelection(inputEText.getText().length() - 7);       //set cursor to point to between()
+            newinput="()^(1/2)";
+            offset=1;
         } else if (s.equals(getString(R.string.buttongenroottext))) {
-            inputEText.append("()^(1/())");
-            inputEText.setSelection(inputEText.getText().length() - 8);
+            newinput="()^(1/())";
+            offset=1;
         } else if (s.equals(getString(R.string.buttonsquaretext))) {
-            inputEText.append("^2");
+            newinput="^2";
         } else if (s.equals(getString(R.string.buttoncubetext))) {
-            inputEText.append("^3");
+            newinput="^3";
         } else if (s.equals(getString(R.string.buttoninversetext))) {
-            inputEText.append("1/()");
-            inputEText.setSelection(inputEText.getText().length() - 1);
+            newinput="(1/())";
+            offset=4;
         } else if (s.equals(getString(R.string.buttongenpowertext))) {
-            inputEText.append("^()");
-            inputEText.setSelection(inputEText.getText().length() - 1);
+            newinput="^()";
+            offset=2;
         } else if (s.equals(getString(R.string.buttonbinomialtext))) {
-            inputEText.append("C(,)");
-            inputEText.setSelection(inputEText.getText().length() - 2);
+            newinput="C(,)";
+            offset=2;
         } else if (s.equals(getString(R.string.buttonplustext)) ||
                 s.equals(getString(R.string.buttonminustext)) ||
                 s.equals(getString(R.string.buttonmultext)) ||
                 s.equals(getString(R.string.buttondivtext)) ||
                 s.equals(getString(R.string.buttonrightparantext)) ||
                 s.equals(getString(R.string.buttonleftparantext))) {       //+-*/()
-            inputEText.append(s);
+            newinput=s;
         } else {                     //for log&trigonometric function
-            inputEText.append(s + "()");
-            inputEText.setSelection(inputEText.getText().length() - 1);
+            newinput=s+"()";
+            offset=s.length()+1;
         }
+        changeinput(newinput,offset);
     }
 
     private void processOperation() {
@@ -129,7 +145,16 @@ public class MainActivity extends AppCompatActivity implements numbersFragment.n
             OutputAns(f);
         }
     }
-
+    private void changeinput(String s,int cursoroffsettochange){
+        String input=inputEText.getText().toString();
+        if(cursoroffsettochange<0){                     //when cursorpositionnotdetermined or messy value
+            cursoroffsettochange=s.length();
+        }
+        int cursorposition=inputEText.getSelectionStart();
+        String newInput=input.substring(0,cursorposition)+s+input.substring(cursorposition);
+        inputEText.setText(newInput);
+        inputEText.setSelection(cursorposition+cursoroffsettochange);
+    }
     private void OutputAns(double f) {
         outputTv.setText("" + f);
     }
